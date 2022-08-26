66ade9213a6da2c4a83e6fd42fb4310fefac1160
Piyush <ppaliwal.89@gmail.com>
2022-08-26 13:29:13 +0530

    updated analytics script

--- components/analytics/Umami.js
+++ components/analytics/Umami.js
@@ -9,7 +9,7 @@ const UmamiScript = () => {
         async
         defer
         data-website-id={siteMetadata.analytics.umamiWebsiteId}
-        src="https://ipiyush-comments.herokuapp.com/umami.js" // Replace with your umami instance
+        src="https://ipiyush-analytics.vercel.app/umami.js" // Replace with your umami instance
       />
     </>
   )
