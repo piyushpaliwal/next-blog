32e38f6f73f0701cc450e2f8cb3ee5a218ac684b
Piyush <ppaliwal.89@gmail.com>
2022-08-26 14:51:23 +0530

    update tracking url

--- components/analytics/Umami.js
+++ components/analytics/Umami.js
@@ -9,7 +9,7 @@ const UmamiScript = () => {
         async
         defer
         data-website-id={siteMetadata.analytics.umamiWebsiteId}
-        src="https://ipiyush-analytics.vercel.app/umami.js" // Replace with your umami instance
+        src="https://ipiyush-analytics.vercel.app/websiteFunctions.js" // Replace with your umami instance
       />
     </>
   )
